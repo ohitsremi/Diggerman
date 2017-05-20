017797f87d18629752391865ff48e5255a2107fe
BornIncompetence <christopher_greer@elcamino.edu>
2017-05-20 14:28:40 -0700

    Diggerman move begone
    alive is now a private data member, will fully implement as common data member later

--- Actor.h
+++ Actor.h
@@ -14,6 +14,7 @@ public:
 		setVisible(true);
 	}	//Does an object below not need to be set to visible?
 	virtual void doSomething(StudentWorld *) = 0;
+	virtual bool isAlive() = 0;
 	virtual ~Actor() {}
 };
 
@@ -29,13 +30,9 @@ public:
 	size_t getWater() { return m_water; }
 	size_t getGold() { return m_gold; }
 	bool getSonar() { return m_sonar; }
-	bool isAlive() { return m_health != 0; }
-	void doSomething(StudentWorld *) override	
-	{
-		if (!isAlive())
-			return;
-	}	
-	void move(StudentWorld*);
+	bool isAlive() override { return m_health != 0; }
+	void increaseGold() { m_gold++; }
+	void doSomething(StudentWorld *) override;
 	virtual ~DiggerMan() {}
 };
 
@@ -45,7 +42,8 @@ class Protester : public Actor
 public:
 	Protester(int x, int y, int ID = IMID_PROTESTER) : Actor(ID, x, y, left, 1.0, 0), m_health(5) {}
 	size_t getHealth() { return m_health; }
-	virtual void doSomething(StudentWorld *) override {}
+	virtual void doSomething(StudentWorld *) override;
+	virtual bool isAlive() override;
 	virtual ~Protester() {}
 };
 
@@ -54,7 +52,8 @@ class HardcoreProtester : public Protester
 	size_t m_health;
 public:
 	HardcoreProtester(int x, int y) : Protester(x, y, IMID_HARD_CORE_PROTESTER), m_health(20) {}
-	virtual void doSomething(StudentWorld *) override {}
+	virtual void doSomething(StudentWorld *) override;
+	virtual bool isAlive() override;
 	virtual ~HardcoreProtester() {}
 };
 
@@ -63,6 +62,7 @@ class Goodie : public Actor
 public:
 	Goodie(int ID, int x, int y) : Actor(ID, x, y, right, 1.0, 2) {}
 	virtual void doSomething(StudentWorld *) = 0;
+	virtual bool isAlive() = 0;
 	virtual ~Goodie() {}
 };
 
@@ -70,59 +70,73 @@ class Sonar : public Goodie
 {
 public:
 	Sonar(int x, int y) : Goodie(IMID_SONAR, x, y) { setVisible(true); }
-	virtual void doSomething(StudentWorld *) override {}
+	virtual void doSomething(StudentWorld *) override;
+	virtual bool isAlive() override;
 	virtual ~Sonar() {}
 };
 
 class Gold : public Goodie
 {
 public:
-	Gold(int x, int y) : Goodie(IMID_GOLD, x, y) {}
-	virtual void doSomething(StudentWorld *) override {}
+	enum GoldState { none, pickup, dropped };
+	Gold(int x, int y, GoldState g) : Goodie(IMID_GOLD, x, y), status(g)
+	{
+		status == pickup ? setVisible(false) : setVisible(true);
+	}
+	virtual void doSomething(StudentWorld *) override;
+	virtual bool isAlive() override{ return alive; }
 	virtual ~Gold() {}
+private:
+	GoldState status;
+	bool alive = true;
 };
 
 class Water : public Goodie
 {
 public:
 	Water(int x, int y) : Goodie(IMID_WATER_POOL, x, y) { setVisible(true); }
-	virtual void doSomething(StudentWorld *) override {}
+	virtual void doSomething(StudentWorld *) override;
+	virtual bool isAlive() override;
 	virtual ~Water() {}
 };
 
 class Oil : public Goodie
 {
-	bool isAlive = true;
+	bool alive = true;
 public:
 	Oil(int x, int y) : Goodie(IMID_BARREL, x, y) { setVisible(false); }
 	virtual void doSomething(StudentWorld *) override;
+	virtual bool isAlive() override{ return alive; }
 	virtual ~Oil() {}
 };
 
 class Boulder : public Actor
 {
+	bool alive = true;
 public:
 	Boulder(int x, int y) : Actor(IMID_BOULDER, x, y, down, 1.0, 1) {}
 	virtual void doSomething(StudentWorld *) override {}
+	virtual bool isAlive() override { return alive; }
 	virtual ~Boulder() {}
 };
 
 class Dirt : public Actor
 {
 public:
-	//Dirt() : Actor(IMID_DIRT, 0, 0, right, 0.25, 3) { setVisible(false); }
 	Dirt(int x, int y) : Actor(IMID_DIRT, x, y, right, 0.25, 3) {}
 	virtual void doSomething(StudentWorld *) override {}
+	virtual bool isAlive() override { return false; }
 	virtual ~Dirt() {}
 };
 
 class Projectile : public Actor
 {
 	int distance=0;
-	bool isAlive = true;
+	bool alive = true;
 public:
 	Projectile(int x, int y, Direction d) : Actor(IMID_WATER_SPURT, x, y, d, 1.0, 1) {}
 	virtual void doSomething(StudentWorld *) override;
+	virtual bool isAlive() override { return alive; }
 	virtual ~Projectile() {}
 };
 #endif // ACTOR_H_
