25f5c53737a3467144fee037812178ca4396ac14
ohitsremi <paripinya_srijun1553@elcamino.edu>
2017-05-19 19:37:57 -0700

    Update StudentWorld.h

--- StudentWorld.h
+++ StudentWorld.h
@@ -26,14 +26,18 @@ public:
 		digger = std::make_shared<DiggerMan>();
 		for (size_t x = 0; x < dirt_field.size(); x++)
 			for (size_t y = 0; y < dirt_field[x].size(); y++)
-				if (y < 8 || (x < 30 || x > 33)) // modified to match his sample
+				if (y < 8 || (x < 30 || x > 33) && y < 60) // modified to match his sample
 					dirt_field[x][y] = std::make_unique<Dirt>(x, y);
 		for (int i = 0; i < std::min((int)getLevel() / 2 + 2, 7); i++)
 		{
 			int x = std::rand() % 60;
 			int y = std::rand() % 36 + 20;	//Between 20 and 56
-			if (isSafeDistanceAway(x, y))
+			if (isSafeDistanceAway(x, y)) {
 				roster.emplace_back(std::make_shared<Boulder>(x, y));
+				for(int i=0; i<4; i++)
+					for(int j=0; j<4; j++)
+					dirt_field[x+i][y+j].reset();
+			}
 			else
 				i--;
 		}
@@ -113,7 +117,7 @@ public:
 	bool isSafeDistanceAway(int x, int y)
 	{
 		for (std::shared_ptr<Actor> a : roster)
-			if (pow(abs(a->getX() - x), 2) + pow(abs(a->getY() - y), 2) <= 36)
+			if (pow(abs(a->getX() - x), 2) + pow(abs(a->getY() - y), 2) <= 36 || !( y < 8 || (x < 26 || x > 33)))
 				return false;
 		return true;
 	}
@@ -131,27 +135,39 @@ public:
 	{
 		//Add implementation, use private data members
 		if (dir == GraphObject::right)
-			x += 2;
+			x += 4;
 		else if (dir == GraphObject::left)
-			x -= 2;
+			x -= 4;
 		else if (dir == GraphObject::up)
-			y += 2;
+			y += 4;
 		else if (dir == GraphObject::down)
-			y -= 2;
+			y -= 4;
 		roster.emplace_back(std::make_shared<Projectile>(x, y, dir));
 	}
 
-	 bool checkObstacle(int x, int y)
+	 bool checkDirtObstacle(int x, int y, GraphObject::Direction d)
 	{
-		if (dirt_field[x][y]->isVisible()==true){
-			return true;
-		}
-		return false;
+		 for (int i=0; i>4; i++)
+			 for (int j = 0; j > 4; j++) {
+				 if (d == GraphObject::right)
+					 if (dirt_field[x+i][y].get() == nullptr)
+						 return false;
+				 else if (d == GraphObject::left)
+					 if (dirt_field[x-i][y].get() == nullptr)
+						 return false;
+				 else if (d == GraphObject::up)
+					 if (dirt_field[x][y+j].get() == nullptr)
+						 return false;
+				 else if (d == GraphObject::down)
+					 if (dirt_field[x][y-j].get() == nullptr)
+						 return false;
+			 }
+		return true;
 	}
 private:
 	std::vector<std::shared_ptr<Actor>> roster;	//maybe also have a 2d array of actor pointers
 	std::shared_ptr<DiggerMan> digger;
-	std::array<std::array<std::unique_ptr<Dirt>, 60>, 64> dirt_field;  // modified to match his sample
+	std::array<std::array<std::unique_ptr<Dirt>, 64>, 64> dirt_field;  // modified to match his sample
 };
 
 #endif // STUDENTWORLD_H_
